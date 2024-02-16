584e7f88cae4dc22e9fcbdaeac86d8cd659a74fb
akhilnandavara <akhiln1108@gmail.com>
2024-02-16 22:33:44 +0530

    Add new routes for fetching restaurant data

--- controllers/Restaurant.js
+++ controllers/Restaurant.js
@@ -9,8 +9,45 @@ exports.getRestaurantList=async(req,res)=> {
 }
 
 exports.getRestaurantData=async(req,res)=> {
+    const {restaurantId}=req.body;
+    const restaurantData=await Restaurant.findOne({_id:restaurantId}).select(" name cuisine images").select({'googleData.ratings':1}).exec();
+    return res.json({
+        success:true,
+        data:restaurantData
+    })
+}
+
+exports.getRestaurantOverview=async(req,res)=> {
     const {restrurantId}=req.body;
-    const restaurantData=await Restaurant.find({_id:restrurantId}).exec();
+    const restaurantData=await Restaurant.findOne({_id:restrurantId}).select({'googleData':1}).exec();
+    return res.json({
+        success:true,
+        data:restaurantData
+    })
+}
+
+exports.getRestaurantMenu=async(req,res)=> {
+    const {restrurantId}=req.body;
+    const restaurantData = await Restaurant.findOne({ _id: restrurantId }).select({ "menu": 1 }).exec();
+
+    return res.json({
+        success:true,
+        data:restaurantData
+    })
+}
+exports.getRestaurantReviews=async(req,res)=> {
+    const {restrurantId}=req.body;
+    const restaurantData = await Restaurant.findOne({ _id: restrurantId }).select({'googleData.reviews':1}).exec();
+
+    return res.json({
+        success:true,
+        data:restaurantData
+    })
+}
+exports.getRestaurantOffers=async(req,res)=> {
+    const {restrurantId}=req.body;
+    const restaurantData = await Restaurant.findOne({ _id: restrurantId }).select("zomatoOffers swiggyOffers magicPinOffers").exec();
+
     return res.json({
         success:true,
         data:restaurantData
--- routes/Restaurant.js
+++ routes/Restaurant.js
@@ -2,12 +2,16 @@ const express =require('express');
 const router = express.Router();
 
 
-const {getRestaurantList,getRestaurantData}=require('../controllers/Restaurant')
+const {getRestaurantList,getRestaurantData, getRestaurantOverview, getRestaurantOffers, getRestaurantReviews, getRestaurantMenu}=require('../controllers/Restaurant')
 
 // ************************************************
 // 1. Add a new route for fetching restaurant data
 // ************************************************
 router.get('/getRestaurantList',getRestaurantList)
 router.post('/getRestaurantData',getRestaurantData)
+router.post('/getRestaurantOverview',getRestaurantOverview)
+router.post('/getRestaurantMenu',getRestaurantMenu)
+router.post('/getRestaurantReviews',getRestaurantReviews)
+router.post('/getRestaurantOffers',getRestaurantOffers)
 
 module.exports=router;
