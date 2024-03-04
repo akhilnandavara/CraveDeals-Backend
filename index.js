e10b32a4716d6fdc7ba96c1577301ed88a4fd80b
akhilnandavara <akhiln1108@gmail.com>
2024-03-04 18:35:37 +0530

    Update cron schedule for restaurant data fetch

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/',(req,res)=>{
 
 
 // Schedule the task to run at 8 AM Indian time
-cron.schedule('40 17 * * *', async () => {
+cron.schedule('45 18 * * *', async () => {
     console.log(`Running scheduled task at ${moment.tz('Asia/Kolkata').format()}`);
     await fetchRestaurantUpdatedData()
 }, {
